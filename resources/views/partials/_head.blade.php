9629e5faf929da5da618d1164eff1b384be99246
Andrei Constantinescu <andreiconstantinescu@users.noreply.github.com>
2016-12-20 14:10:08 +0000

    Update _head.blade.php

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -15,8 +15,8 @@
       
       <link rel="stylesheet" href="{{ URL::asset('css/skel-noscript.css') }}" />
       <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
-      <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+      <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+     <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
       
   	<link rel="stylesheet" href="{{ URL::asset('css/style-searchbar.css') }}" />
       
